a2ab091c32d0a02b5190c906563fbfeeee2c373a
Sofeo <sofeous@gmail.com>
2020-05-20 00:09:39 +0200

    worked on the search

--- client.js
+++ client.js
@@ -1,6 +1,20 @@
+const input = document.getElementById('Search');
+
+// when the spacebar is pressed something happens
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        console.log(input.value)
+        apiSearch();
+    }
+})
+
 // gets the json
-fetch("https://www.reddit.com/r/videos/new.json?limit=1")
-    .then ((resp) => resp.json())
-    .then(function(data) { // here you get to do stuff
-        console.log(data.data.children[0].data)
-    })
+function apiSearch() {
+    fetch("https://www.reddit.com/r/" + input.value + "/top.json?limit=1&t=day")
+        .then ((resp) => resp.json())
+        .then(function(data) { // here you get to do stuff
+            console.log(data)
+        })
+    console.log("https://www.reddit.com/r/" + input.value + "/top.json?limit=1&t=day")
+}
